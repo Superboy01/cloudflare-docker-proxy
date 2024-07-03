247694eaef727df0e302a023faa2f9f0f68a4a12
Superboy01 <142149607+Superboy01@users.noreply.github.com>
2024-07-03 11:30:31 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.woil.cn": dockerHub,
+  "quay.woil.cn": "https://quay.io",
+  "gcr.woil.cn": "https://gcr.io",
+  "k8s-gcr.woil.cn": "https://k8s.gcr.io",
+  "k8s.woil.cn": "https://registry.k8s.io",
+  "ghcr.woil.cn": "https://ghcr.io",
+  "cloudsmith.woil.cn": "https://docker.cloudsmith.io",
+  "ecr.woil.cn": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
